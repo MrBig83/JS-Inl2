508bd16ee61f579452e57dac41e77e8b8dd9e881
MrBig83 <mjohansson176@gmail.com>
2022-09-29 00:39:46 +0200

    Added some DOM man.

--- script.js
+++ script.js
@@ -7,7 +7,10 @@ let verify;
 let findpassword;
 let pwd;
 let userIs;
-let userIsLoggedIn = false;
+let userIsLoggedIn;
+let p;
+let logo;
+
 
 const btn = document.querySelector("#button");
 
@@ -31,7 +34,6 @@ btn.addEventListener("click", (e) => {
     } else {
         console.log("Användaren finns INTE");
         userIs = false;
-        
     }
     
 // ----- Login & verify in console ----- //
@@ -43,6 +45,83 @@ btn.addEventListener("click", (e) => {
         console.log("Felaktigt användarnamn eller lösenord");
         userIsLoggedIn = false;   
         console.log(userIsLoggedIn);
+        //SKAPA P med texten Felaktigt användarnamn eller lösenord -------------------------------------------------------------------------------//
+    }
+    console.log(userIsLoggedIn);
+
+    if (userIsLoggedIn === true) {
+        hideLogin();
+        welcomeText();
     }
 });
+// ----- LOG OUT ----- //
+console.log(userIsLoggedIn);
+const logoutBtn = document.querySelector("#logOut");
+logoutBtn.addEventListener("click", (e) => {
+    e.preventDefault()
+if (userIsLoggedIn === true){
+    userIsLoggedIn = false;
+    console.log(userIsLoggedIn);
+}
+showLogin()
+});
+// ----- CREATE ACCOUNT ----- // JOBBA PÅ DENNA ---------------------------------------------------------------------------------------------------//
+const createAccountBtn = document.querySelector("#createAccountBtn");
+createAccountBtn.addEventListener("click", (e) => {
+    e.preventDefault()
+    showCreateAccount();
 
+});
+
+// ----- FUNCTIONS ----- //
+function hideLogin(){
+    
+    const form = document.querySelector("form");
+    form.style.display = "none";
+    
+
+    const logout = document.getElementById("logoutBtn")
+    console.log(logout);
+    logout.style.display="inline";
+    console.log(userIsLoggedIn);
+};
+function welcomeText(){
+    logo = document.querySelector("h1");
+    logo.style.color = ("green");
+    p = document.querySelector("p");
+    p.textContent = ("Välkommen, " + nameInput + "!");
+}
+
+function showLogin(){
+    const form = document.querySelector("form");
+    form.style.display = "block";
+    form.inputUsername.value = "";
+    form.inputPassword.value = "";
+    p = document.querySelector("p");
+    p.textContent = ("Log in here:");
+    logo = document.querySelector("h1");
+    logo.style.color = ("black");
+
+    const logout = document.getElementById("logoutBtn")
+    console.log(logout);
+    logout.style.display="none";
+    console.log(userIsLoggedIn);
+};
+
+
+
+
+function showCreateAccount(){
+    hideLogin()
+    const createAccountForm = document.createElement("form");
+    createAccountForm.appendChild.innerHTML = "<input type=text>"; 
+    logo = document.querySelector("h1");
+    logo.style.color = ("yellow");
+    logo.style.textShadow = ("3px 3px 3px black");
+    p = document.querySelector("p");
+    p.textContent = ("Kul att du vill skapa ett konto! Fyll i formuläret nedan, så är du snart igång!");
+    const logout = document.getElementById("logoutBtn")
+    console.log(logout);
+    logout.style.display="none"
+
+}

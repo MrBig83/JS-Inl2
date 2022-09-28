c1f8a55566d4c1c63a7f22af6364716dee7c28a2
MrBig83 <mjohansson176@gmail.com>
2022-09-28 22:46:38 +0200

    Code clean-up

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//console.log("Ööööös");
+// ----- INIT VARIABLES ----- //
 let nameInput = "";
 let pwdInput = "";
 let username = "";
@@ -8,53 +8,22 @@ let findpassword;
 let pwd;
 let userIs;
 let userIsLoggedIn = false;
+
 const btn = document.querySelector("#button");
 
-
-/*
-const myUserList = {
-    user : "AAA",
-    pwd : "12345"
-
- };
-*/
-
-
-// ---- TEST CABIN ---- //
-
-//console.log(theUserList.indexOf(nameInput));
-//console.log(nameInput.value);
-console.log(nameInput);
-console.log(pwdInput);
-console.log(findpassword);
-console.log(verify);
-console.log(document.querySelector("#inputUsername").value);
-
-
-
-
-// ---- TEST CABIN ENDS ---- //
+// ----- LOGIN FUNCTION STARTS HERE ----- //
 
 btn.addEventListener("click", (e) => {
     e.preventDefault()
+        nameInput = document.querySelector("#inputUsername").value;
+        pwdInput = document.querySelector("#inputPassword").value;
 
-    nameInput = document.querySelector("#inputUsername").value;
-    pwdInput = document.querySelector("#inputPassword").value;
-    console.log(nameInput);
-    console.log(pwdInput);
-
+// ----- Check if the user exists ----- //
     theUserList = ["AAA", "12345", "BBB", "54321"];
     let verify = (theUserList.indexOf(nameInput));
     findpassword = (verify+1);
-    console.log(theUserList[findpassword]);
-    
     pwd = theUserList[findpassword];
-    console.log(pwd);
     
-
-    
-
-
     if(theUserList.includes(nameInput))
     {
         console.log("Användaren finns");
@@ -64,31 +33,16 @@ btn.addEventListener("click", (e) => {
         userIs = false;
         
     }
-    console.log(userIs);
     
-
+// ----- Login & verify in console ----- //
     if(userIs === true && pwdInput === pwd)
     {
         console.log("Inloggad!")
         userIsLoggedIn = true;
-
     } else {
         console.log("Felaktigt användarnamn eller lösenord");
-        userIsLoggedIn = false;
-        
-        console.log(nameInput);
-        console.log(pwdInput);
+        userIsLoggedIn = false;   
         console.log(userIsLoggedIn);
     }
-
-
 });
 
-console.log(nameInput);
-
-    /*
-    const username = document.querySelector("inputUsername").value;    
-    console.log(username);
-    */
-
-//console.log(theUserList.includes(nameInput));
